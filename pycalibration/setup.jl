4fa7cc81132ba263b739b1a1c303c73e28959df2
David Widmann <david.widmann@it.uu.se>
2022-01-28 01:48:36 +0100

    Fix wrappers

--- pycalibration/setup.jl
+++ pycalibration/setup.jl
@@ -4,8 +4,8 @@ using PyCall: PyCall
 # use Julia wrapper for ColVecs and RowVecs in Python
 # otherwise the automatic conversions in PyCall will convert it to a matrix!
 function PyCall.PyObject(::Type{<:ColVecs})
-    return PyCall.pyfunctionret(KernelFunctions.ColVecs, Any, PyCall.PyAny)
+    return PyCall.pyfunctionret(ColVecs, Any, PyCall.PyAny)
 end
 function PyCall.PyObject(::Type{<:RowVecs})
-    return PyCall.pyfunctionret(KernelFunctions.RowVecs, Any, PyCall.PyAny)
+    return PyCall.pyfunctionret(RowVecs, Any, PyCall.PyAny)
 end
