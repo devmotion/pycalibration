5d13755b7449af516e316c58f6a41142af037712
David Widmann <devmotion@users.noreply.github.com>
2022-02-12 01:46:36 +0100

    Load module

--- pycalibration/ca.py
+++ pycalibration/ca.py
@@ -5,4 +5,5 @@ import os
 script_dir = os.path.dirname(os.path.realpath(__file__))
 Main.include(os.path.join(script_dir, "PyCalibration.jl"))
 
+from julia import PyCalibration
 sys.modules[__name__] = PyCalibration
